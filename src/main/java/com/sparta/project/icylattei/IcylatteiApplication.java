17e97dbc3535a98494a2cabb2ffee427322a1873
DBuu_Kong <feel00k0810@gmail.com>
2024-02-23 12:30:28 +0900

    :sparkles: initial commit

---
+++ src/main/java/com/sparta/project/icylattei/IcylatteiApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.project.icylattei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcylatteiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcylatteiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/project/icylattei/IcylatteiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.project.icylattei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcylatteiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
