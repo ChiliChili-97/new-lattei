dc697f39fe55196a5a089d154fde3b942cd1c9fb
DBuu_Kong <feel00k0810@gmail.com>
2024-02-23 13:18:34 +0900

    :sparkles: Add User Entity

---
+++ src/main/java/com/sparta/project/icylattei/user/entity/User.java
@@ -0,0 +1,29 @@
+package com.sparta.project.icylattei.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+}
