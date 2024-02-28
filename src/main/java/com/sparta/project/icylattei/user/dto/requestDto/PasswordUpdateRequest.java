f7136e4d3bc2b40c2c8dfc16b5d9e0ebed1b8971
Oh YeonJoo <109337974+oyeonjoo@users.noreply.github.com>
2024-02-28 10:43:59 +0900

    Feat/profile (#12)
    * :sparkles: 프로필 조회 및 수정
    
    * :sparkles: 비밀번호 수정
    
    * :art: 코드 컨벤션
    
    * :sparkles: 비밀번호 수정
    
    * :sparkles: custom exception 추가
    
    * :del: CommonTest 파일 삭제

--- src/main/java/com/sparta/project/icylattei/user/dto/requestDto/PasswordUpdateRequest.java
+++ src/main/java/com/sparta/project/icylattei/user/dto/requestDto/PasswordUpdateRequest.java
@@ -7,6 +7,12 @@ import lombok.Getter;
 @Getter
 public class PasswordUpdateRequest {
     @NotBlank(message = "비밀번호는 필수로 입력해야 합니다.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\W)(?=.*\\d).{8,15}$")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\W)(?=.*\\d).{8,15}$",
+        message = "비밀번호는 최소 8자 이상, 15자 이하로 알파벳과 특수문자, 숫자로 구성되어야 합니다.")
     private String password;
+
+    @NotBlank(message = "비밀번호는 필수로 입력해야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\W)(?=.*\\d).{8,15}$",
+        message = "비밀번호는 최소 8자 이상, 15자 이하로 알파벳과 특수문자, 숫자로 구성되어야 합니다.")
+    private String checkPassword;
 }
