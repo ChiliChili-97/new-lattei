35a237cddc22192341f4ffda59a817e9dea8fb75
DBuu_Kong <71073365+ChiliChili-97@users.noreply.github.com>
2024-02-28 12:36:31 +0900

    Tests/review (#14)
    * :art: 불필요한 로직 삭제
    
    * :art: 불필요한 로직 삭제
    
    * :memo: 어드민, 상품 쿼리 추가
    
    * :bug: 리뷰 로직 변경
    
    * :art: 로그인 안한 유저도 리뷰를 확인할 수 있게 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,8 @@
-INSERT INTO Users () VALUES ()
+INSERT INTO user (created_at, info, modified_at, nickname, password, role, username) values (now(), '', now(), 'ADMIN', 'administrator123*!#%', 'ADMIN', 'lattei_admin01');
+INSERT INTO product (product_name, price) VALUES ('아메리카노', '4500');
+INSERT INTO product (product_name, price) VALUES ('라떼', '4500');
+INSERT INTO product (product_name, price) VALUES ('카푸치노', '5500');
+INSERT INTO product (product_name, price) VALUES ('프라푸치노', '5500');
+INSERT INTO product (product_name, price) VALUES ('허니 브레드', '6500');
+INSERT INTO product (product_name, price) VALUES ('플레인 베이글', '5000');
+INSERT INTO product (product_name, price) VALUES ('크로크무슈', '7000');
